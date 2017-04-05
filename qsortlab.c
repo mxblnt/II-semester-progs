ebf585703badcb438b92895610e590eddb28a2e3
root <root@ip-192-168-0-103.is74.loc>
2017-04-05 08:36:46 +0500

    2 new labs: 1) qsort() on randomly generated array 2) changing register in strings

---
+++ qsortlab.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <search.h>
+#include <string.h>
+#include <time.h>
+
+void PrintArray(int * SomeArray, int ArraySize)
+{
+  int i;
+
+  printf("A = [ ");
+  for (i = 0; i < ArraySize; i++) printf("%d ", SomeArray[i]);
+  printf("]\n");
+}
+
+int compare (const void * a, const void * b)
+{
+  return (*(int*)b - *(int*)a);
+}
+
+int main()
+{
+  int ArraySize, i;
+  srand(time(0));
+
+  printf("Enter array size: ");
+  scanf("%d", &ArraySize);
+
+  int * NewArray = (int*) malloc (ArraySize);
+
+  for (i = 0; i < ArraySize; i++) NewArray[i] = rand() % 100;
+
+  PrintArray(NewArray, ArraySize);
+  qsort(NewArray, ArraySize, sizeof(int), compare);
+  PrintArray(NewArray, ArraySize);
+
+  free(NewArray);
+  return 0;
+}
---
+++ stringlab1.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+void changeRegister(char * stringX)
+{
+  for (int i = 0; i < (int)strlen(stringX); i++) {
+    if (islower(stringX[i])) stringX[i] = toupper(stringX[i]);
+    else stringX[i] = tolower(stringX[i]);
+  }
+}
+
+int main()
+{
+  char stringA[256];
+
+  printf("Your input: ");
+  fgets(stringA, 256, stdin);
+  changeRegister(stringA);
+  printf("Output: %s\n", stringA);
+
+  return 0;
+}
